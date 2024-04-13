dad50e9a99910e7b1e4163afc1bd5b504e66ccf8
Taj Ikhlaas <tajikhlaas@ksu.edu>
2024-04-13 00:07:15 -0500

    Initial Commit

---
+++ HackKU24/HackKU24App.swift
@@ -0,0 +1,17 @@
+//
+//  HackKU24App.swift
+//  HackKU24
+//
+//  Created by Tajammul Ikhlaas on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HackKU24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
